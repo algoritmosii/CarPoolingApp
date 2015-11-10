080f163c38e5f8b7e0964f29a3c3b533f4d5cfea
lauraperez <lauraperez.code@gmail.com>
2015-11-10 00:46:13 -0200

    agrego test unitario para registrar varias esquinas(x,y) en el mapa

--- AED2/src/test/TestMapa.java
+++ AED2/src/test/TestMapa.java
@@ -2,6 +2,8 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,11 +18,11 @@ public class TestMapa {
 	@Before
 	public void contexto() {
 		sistema = new Sistema();
-
 	}
 
 	@Test
 	public void testA_InicializarSistema() {
+		
 		System.out.println(" test: inicializarSistema");
 		ISistema sistema;
 		sistema = new Sistema();
@@ -33,6 +35,7 @@ public class TestMapa {
 
 	@Test
 	public void testB_RegistrarEsquina() {
+		
 		System.out.println(" test: registrarEsquina");
 		ISistema sistema;
 		sistema = new Sistema();
@@ -42,9 +45,66 @@ public class TestMapa {
 			assertEquals(Retorno.Resultado.OK, ret2.resultado);
 		}
 	}
-	
+
+	/*
 	@Test
-	public void testC_DestruirSistema() {
+	public void testB_RegistrarEsquinasVarias() {
+		
+		System.out.println(" test: registrarEsquinasVarias");
+		ISistema sistema;
+		sistema = new Sistema();
+		Retorno ret = sistema.inicializarSistema(10);
+		if (ret.resultado.equals(Retorno.Resultado.OK)) {
+
+			Double uniORTX = -34.9050291;
+			Double uniORTY = -56.1915827;
+			Retorno ret1 = sistema.registrarEsquina(uniORTX, uniORTY);
+			assertEquals(Retorno.Resultado.OK, ret1.resultado);
+
+			Double immX = -34.9050291;
+			Double immY = -56.1915817;
+			Retorno ret2 = sistema.registrarEsquina(immX, immY);
+			assertEquals(Retorno.Resultado.OK, ret2.resultado);
+
+			Double teatroSolisX = -34.9072423;
+			Double teatroSolisY = -56.2005342;
+			Retorno ret3 = sistema.registrarEsquina(teatroSolisX, teatroSolisY);
+			assertEquals(Retorno.Resultado.OK, ret3.resultado);
+
+			Double facArquitecturaX = -34.913232;
+			Double facArquitecturaY = -56.167944;
+			Retorno ret4 = sistema.registrarEsquina(facArquitecturaX, facArquitecturaY);
+			assertEquals(Retorno.Resultado.OK, ret4.resultado);
+
+			Double tabernaDiabloX = -34.9124532;
+			Double tabernaDiabloY = -56.1693436;
+			Retorno ret5 = sistema.registrarEsquina(tabernaDiabloX, tabernaDiabloY);
+			assertEquals(Retorno.Resultado.OK, ret5.resultado);
+
+			Double mNAVX = -34.913442;
+			Double mNAVY = -56.1678003;
+			Retorno ret6 = sistema.registrarEsquina(mNAVX, mNAVY);
+			assertEquals(Retorno.Resultado.OK, ret6.resultado);
+
+			Double aeropuertoX = -34.8621083;
+			Double aeropuertoY = -56.0613573;
+			Retorno ret7 = sistema.registrarEsquina(aeropuertoX, aeropuertoY);
+			assertEquals(Retorno.Resultado.OK, ret7.resultado);
+
+			Double solisChicoX = -34.7535082;
+			Double solisChicoY = -55.7126396;
+			Retorno ret8 = sistema.registrarEsquina(solisChicoX, solisChicoY);
+			assertEquals(Retorno.Resultado.OK, ret8.resultado);
+
+			Double facQuimicaX = -34.8883007;
+			Double facQuimicaY = -56.1850452;
+			Retorno ret9 = sistema.registrarEsquina(facQuimicaX,facQuimicaY);
+			assertEquals(Retorno.Resultado.OK, ret9.resultado);
+		}
+	}
+*/
+	@Test
+	public void testD_DestruirSistema() {
 		System.out.println(" test: destruirSistema");
 		ISistema sistema;
 		sistema = new Sistema();
